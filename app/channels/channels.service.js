feca58000ff34836be980f32dffb6c8e6cbc7868
Nicholas Hunkins <nicholas.hunkins@graybar.com>
2018-10-13 18:46:18 -0500

    firstcommit

---
+++ app/channels/channels.service.js
@@ -0,0 +1,8 @@
+angular
+  .module("angularfireSlackApp")
+  .factory("Channels", function($firebaseArray) {
+    var ref = firebase.database().ref("channels");
+    var channels = $firebaseArray(ref);
+
+    return channels;
+  });
